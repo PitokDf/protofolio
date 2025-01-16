50275c9e135da72b7d75c639681351c4d31953c5
Pito Desri Pauzi <povat0171@gmail.com>
2025-01-16 23:27:13 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,39 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import { ThemeProvider } from "@/components/theme-provider";
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Pito Desri Pauzi - Protofolio',
   description: 'Portfolio website showcasing my work and experience as a Full Stack Developer',
+  keywords: [
+    'Full Stack Developer', 'Web Developer', 'Frontend', 'Backend', 'React', 'Next.js', 'Laravel', 'Node.js', 'Prisma', 'PostgreSQL', 'MySQL', 'JavaScript', 'TypeScript', 'API Development', 'Database Management', 'Version Control', 'GitHub', 'User Authentication', 'JWT'
+  ],
+  authors: [{
+    name: 'Pito Desri Pauzi', url: 'https://github.com/PitokDf'
+  }],
+  openGraph: {
+    title: 'Pito Desri Pauzi - Protofolio',
+    description: 'Portfolio website showcasing my work and experience as a Full Stack Developer',
+    url: 'https://protofolio-git-main-pitok-dfs-projects.vercel.app/',
+    siteName: 'Pito Desri Pauzi - Protofolio',
+    images: ['https://protofolio-git-main-pitok-dfs-projects.vercel.app/pito-desri-pauzi.png'],
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      "max-snippet": -1
+    },
+  },
+  verification: {
+    google: 'google8156d5c26713cfcd'
+  }
 };
 
 export default function RootLayout({
